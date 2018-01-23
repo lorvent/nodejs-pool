efb9d7025c0326dfad9c43b3212a8b27772de580
1rV1N <34376228+1rV1N-git@users.noreply.github.com>
2018-01-23 14:46:29 +0300

    Update deploy.bash

--- deployment/deploy.bash
+++ deployment/deploy.bash
@@ -26,7 +26,7 @@ sudo systemctl enable ntp
 cd /usr/local/src
 sudo cp ~/nodejs-pool/deployment/coindaemon.service /lib/systemd/system/
 sudo useradd -m coindaemon -d /home/coindaemon
-sudo systemctl coindaemon-reload
+sudo systemctl daemon-reload
 sudo systemctl enable coindaemon
 sudo systemctl start coindaemon
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
@@ -79,7 +79,7 @@ pm2 install pm2-logrotate &
 mysql -u root --password=$ROOT_SQL_PASS < deployment/base.sql
 mysql -u root --password=$ROOT_SQL_PASS pool -e "INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('api', 'authKey', '`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`', 'string', 'Auth key sent with all Websocket frames for validation.')"
 mysql -u root --password=$ROOT_SQL_PASS pool -e "INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('api', 'secKey', '`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`', 'string', 'HMAC key for Passwords.  JWT Secret Key.  Changing this will invalidate all current logins.')"
-pm2 start init.js --name=api --log-date-format="YYYY-MM-DD HH:mm Z" -- --module=api
+pm2 start init.js --name=api --log-date-format="YYYY-MM-DD HH:mm:ss Z" -- --module=api
 bash ~/nodejs-pool/deployment/install_lmdb_tools.sh
 cd ~/nodejs-pool/sql_sync/
 env PATH=$PATH:`pwd`/.nvm/versions/node/v8.9.3/bin node sql_sync.js
